ab2e0c84ef05fa1f644a27b20281fc5e65ca346a
DaiSales <daisales35@gmail.com>
2025-06-03 20:35:15 -0300

    catalogo

--- resources/views/catalogo.blade.php
+++ resources/views/catalogo.blade.php
@@ -7,7 +7,7 @@
                 <div class="col-5 area-item-catalogo"> <img class="img-catalogo" src="{{ Vite::asset('resources/asset/images/teste-catalogo.jpg') }}"></div>
                 <div class="col-7 flex-row">
                     <h2>Cuidados pós transplante</h2>
-                    <p class="p-5">Produtos indicados para os primeiros dias/semanas após o procedimento, com fórmulas suaves e propriedades cicatrizantes.</p>
+                    <p class="p-5">Produtos indicados para os primeiros dias/semanas após o procedimento, com fórmulas suaves e propriedades cicatrizantes. Nos primeiros dias e semanas após um transplante capilar, é essencial usar produtos suaves, não irritantes e com propriedades cicatrizantes e calmantes para garantir uma boa recuperação e promover o crescimento saudável dos folículos transplantados.</p>
                 </div>
             </div>
         </div>
@@ -16,7 +16,11 @@
             <div class="row d-flex align-items-center">
       <div class=" col-7 flex-row">
                 <h2>Crescimento e Fortalecimento</h2>
-                <p class="p-5">Produtos para estimular o crescimento dos fios e fortalecer os enxertos após o período inicial de cicatrização.</p>
+                <p class="p-5">Produtos para estimular o crescimento dos fios e fortalecer os enxertos após o período inicial de cicatrização. Após o período inicial de cicatrização (geralmente após 15 a 30 dias do transplante capilar), é possível introduzir produtos que estimulem o crescimento dos fios e fortaleçam os enxertos, sempre com orientação médica.
+
+Principais tipos de produtos recomendados: 1. Tônicos Capilares Estimulantes; 2. Shampoo Fortalecedor; 3. Suplementos Orais; 4. Séruns ou Ampolas de Crescimento
+
+</p>
             </div>
             <div class="col-5"> <img class="img-catalogo" src="{{ Vite::asset('resources/asset/images/teste-catalogo.jpg') }}" alt="imagem do catálogo"></div>
         </div>
@@ -28,7 +32,9 @@
             <div class="col-5 area-item-catalogo"> <img class="img-catalogo" src="{{ Vite::asset('resources/asset/images/teste-catalogo.jpg') }}"></div>
             <div class="col-7 flex-row">
                 <h2>Suplementos Capilares</h2>
-                <p class="p-5">Vitaminas e minerais essenciais para crescimento saudável dos fios.</p>
+                <p class="p-5">Vitaminas e minerais essenciais para crescimento saudável dos fios. Para um crescimento saudável dos fios, especialmente após um transplante capilar, o organismo precisa estar bem nutrido. Os principais nutrientes (vitaminas e minerais) que desempenham um papel fundamental na saúde capilar: Vitaminas do Complexo B (B1, B2, B3, B5, B6, B12); Vitaminas B7, D, E; Zinco; Ferro; Silício Orgânico; Ácido Fólico (Vitamina B9); 
+
+</p>
             </div>
         </div>
     </div>
